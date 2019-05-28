cdc3fce1ca51d1bd93b9bc1b122c22a0f617e666
Samuel Leong <leowhiz@yahoo.com.sg>
2019-05-28 23:31:27 +0800

    Got database to send anand retrieve data

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
+const port = process.env.PORT || 3001;
+
 const express = require('express');
-const bodyParser= require('body-parser')
 const app = express();
 
-const port = 3001 | process.env.PORT;
+//Handling MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const db_url = "mongodb+srv://admin:admin@pandas-ef9ir.mongodb.net/test?retryWrites=true";
 
-app.use(bodyParser.urlencoded({extended: true}));
+const client = new MongoClient(db_url, { useNewUrlParser: true });
+let db;
+const COLLECTION_NAME = "votes";
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html'); //note, path must be absolute
-})
+client.connect(err => {
+	if(err) return console.log("ERROR: "+err);
 
-app.post('/form1', (req, res)=>{
-	console.log(req.body);
-})
+	db = client.db("cuddly-animals");
 
-app.use(express.static('public'))
-
-app.listen(port, () => {
-	console.log("Listening on http://localhost:" + port);
+	console.log("Connected to MongoDB server at "+db_url);
+	app.listen(port, () => {
+		console.log("Listening on http://localhost:" + port);
+	});
 });
+
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+//Handling index.html and subsequent votes
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html'); //note, path must be absolute
+
+	console.log("Hello!");
+
+	db.collection(COLLECTION_NAME).find().toArray((err, results) => {
+		if(err) return console.log("ERROR: "+err);
+
+		console.log("Obtained from database: "+JSON.stringify(results))
+		// send HTML file populated with quotes here
+	});
+});
+
+app.post('/', (req, res) => {
+	console.log("Hello?!?");
+});
+
+//Handling forms
+app.post('/form1', (req, res)=>{
+	db.collection(COLLECTION_NAME).insertOne(req.body, (err, result) => {
+		if(err) return console.log("ERROR: "+err);
+
+		console.log('Saved to database: '+result)
+		res.redirect('/')
+	})
+})
